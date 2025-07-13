f1ef35bee00995a233a1ec9edeb7afee3bc769ee
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-07-12 23:55:22 -0500

    [Feat] Misc Updoots * Port to 1.21.7 * Fix mixin etiquette * push gradle wrapper * don't depend on fapi * remove comments from build.gradle

--- src/main/java/net/snackbag/anvils/anvilsound/mixin/WorldMixin.java
+++ src/main/java/net/snackbag/anvils/anvilsound/mixin/WorldMixin.java
@@ -1,31 +1,27 @@
 package net.snackbag.anvils.anvilsound.mixin;
 
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.snackbag.anvils.anvilsound.AnvilsResounded;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.world.World;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-    @Shadow public abstract void playSoundAtBlockCenter(BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance);
 
-    @Inject(method = "playSoundAtBlockCenter", at = @At("HEAD"), cancellable = true)
-    private void onProcessWorld(BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch, boolean useDistance, CallbackInfo ci) {
-        if (sound == SoundEvents.BLOCK_ANVIL_USE) {
-            if (AnvilsResounded.isEnchanting != null) {
-                if (AnvilsResounded.isEnchanting) {
-                    ci.cancel();
-                    AnvilsResounded.isEnchanting = null;
-                    playSoundAtBlockCenter(pos, SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, category, volume, pitch, useDistance);
-                }
-            }
-        }
-    }
+	@ModifyVariable(method = "playSoundAtBlockCenterClient", at = @At("HEAD"), argsOnly = true, index = 2)
+	private SoundEvent onProcessWorld(SoundEvent sound) {
+		if (sound == SoundEvents.BLOCK_ANVIL_USE) {
+			if (AnvilsResounded.isEnchanting != null) {
+				if (AnvilsResounded.isEnchanting) {
+					AnvilsResounded.isEnchanting = null;
+					return SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE;
+				}
+			}
+		}
+		return sound;
+	}
 }
